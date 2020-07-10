152875999c7a511b5549a401a7e42fd66c5b0cc8
dvij542 <43860166+dvij542@users.noreply.github.com>
2020-07-10 11:31:41 +0530

    Tested code with min error 1.322

--- layers/graph_operation_layer.py
+++ layers/graph_operation_layer.py
@@ -14,10 +14,38 @@ class ConvTemporalGraphical(nn.Module):
         super().__init__()
 
         self.kernel_size = kernel_size
+        self.adjmatder = nn.Sequential(
+                nn.Conv2d(
+                    7,
+                    16,
+                    kernel_size = 1,
+                    stride=(1,1)),
+                #nn.BatchNorm2d(16),
+                nn.ReLU(inplace=False),
+                nn.Dropout(0.5, inplace=False),
+                nn.Conv2d(
+                    16,
+                    32,
+                    kernel_size = 1,
+                    stride=(1,1)),
+                #nn.BatchNorm2d(16),
+                nn.ReLU(inplace=False),
+                nn.Dropout(0.5, inplace=False),
+                nn.Conv2d(
+                    32,
+                    64,
+                    kernel_size = 1,
+                    stride=(1,1)),
+                #nn.BatchNorm2d(16),
+                #nn.BatchNorm2d(16),
+                nn.ReLU(inplace=False),
+                nn.Dropout(0.5, inplace=False)
+
+            )
         # To increase the no of channels of the graph to out_channels*k
         self.conv = nn.Conv2d(
             in_channels,
-            out_channels * kernel_size,
+            out_channels ,
             kernel_size=(t_kernel_size, 1),
             padding=(t_padding, 0),
             stride=(t_stride, 1),
@@ -25,13 +53,23 @@ class ConvTemporalGraphical(nn.Module):
             bias=bias)
 
     def forward(self, x, A):
+        
         assert A.size(1) == self.kernel_size
         x = self.conv(x)
+        mask = A[:,7:]
+        A = self.adjmatder(A[:,:7])
+        A = A*mask
+        Dl = ((A.sum(axis=2) + 0.001)**(-1)).float()
+        A = torch.einsum('ncvw,ncw->ncvw',(A,Dl))
+        
         # To increase the no of channels of the graph to out_channels*k
-        n, kc, t, v = x.size()
+        n, c, t, v = x.size()
         # x is now a 5d tensor with size (N,k,c,t,v)
-        x = x.view(n, self.kernel_size, kc//self.kernel_size, t, v)
+        #x = x.view(n, c, t, v)
+        #A is (n,64,v,v)
         # Matrix multiplication followed by addition of all individual kernel matrices
-        x = torch.einsum('nkctv,nkvw->nctw', (x, A))
+        #print(x.shape)
+        #print(A.shape)
+        x = torch.einsum('nctv,ncvw->nctw', (x, A))
 
         return x.contiguous(), A
