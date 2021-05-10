47ede1ae31c4877ff6a7ce964aa22cbe106af3cb
Dvij Kalaria <dvij.kalaria@gmail.com>
2021-05-10 19:57:31 +0000

    output is squeezed fix that

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ def my_load_model(pra_model, pra_path):
 # custom dataloader and dataset class
 def data_loader(pra_path, pra_batch_size=128, pra_shuffle=False, pra_drop_last=False, train_val_test='train'):
 	feeder = Feeder(data_path=pra_path, graph_args=graph_args, train_val_test=train_val_test)
-	print(f"first adj matrix")
-	feeder[0]
+	# print(f"first adj matrix")
+	# feeder[0]
 	loader = torch.utils.data.DataLoader(
 		dataset=feeder,
 		batch_size=pra_batch_size,
