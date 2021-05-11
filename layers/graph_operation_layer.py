21f660f65a7bd4dae4aff52176d5f120ce44528c
abhi123-iitian <72932189+abhi123-iitian@users.noreply.github.com>
2021-05-12 01:17:20 +0530

    Update graph_operation_layer.py

--- layers/graph_operation_layer.py
+++ layers/graph_operation_layer.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import os 
+import torch.nn.functional as F
+import os
 CUDA_VISIBLE_DEVICES='0'
 os.environ["CUDA_VISIBLE_DEVICES"] = CUDA_VISIBLE_DEVICES
-dev = 'cuda:0' 
+dev = 'cuda:0'
+CUDA_LAUNCH_BLOCKING=1
 
 
 class ConvTemporalGraphical(nn.Module):
@@ -19,7 +21,6 @@ class ConvTemporalGraphical(nn.Module):
 				 bias=True):
 		super().__init__()
 		self.x1 = None
-		self.x2 = None
 
 		self.kernel_size = kernel_size
 		self.adjmatder = nn.Sequential(
@@ -59,50 +60,104 @@ class ConvTemporalGraphical(nn.Module):
 			stride=(t_stride, 1),
 			dilation=(t_dilation, 1),
 			bias=bias)
-		self.l1 = nn.Linear(2,1)
+		self.l1 = nn.Linear(128,1)
 		self.l2 = nn.Linear(6,1)
-		self.l3 = nn.Linear(2,1)
-		self.relu= nn.LeakyReLU(0.2)
-		self.softmax = nn.Softmax2d()
+		#self.l3 = nn.Linear(2,1)
+		#self.relu= nn.LeakyReLU(0.2)
+		self.softmax = nn.Softmax(dim=2)
+		
+		
+		# LeakyReLU
+		self.relu = nn.LeakyReLU(0.2,inplace=False)
 
 
 	def forward(self, x, A):
 		assert A.size(1) == self.kernel_size
 		x = self.conv(x)
 		mask = A[:,7:]
+		#print(mask.shape)
 
-		A = self.adjmatder(A[:,:7])
+		#A = self.adjmatder(A[:,:7])
+		A = A[:,:7]
+		x1 = x.permute(0,3,1,2)
+		#print(x1.shape)
+		n, v, c, t = x1.size()
+		x1 = x1.reshape(n*v*c,t)
+		x1 = x1.to(dev)
+		#print(x1.shape)
+		x1 = self.l2(x1)
+		x1 = x1.reshape(n,v,c)
+		#x1 = self.l1(x1)
+		#x1 = x1.reshape(n, v, 7)s
+		#A = A.permute(0,2,3,1)
+		#x2 = torch.matmul(torch.transpose(x1,0,1),x1)
+		x2 = torch.ones([n, v, v, c], dtype=torch.float64)
+		#print(x2.shape)
+		x3 = torch.ones([n, v, v, c], dtype=torch.float64)
+		x2 = x2.to(dev)
+		x3 = x3.to(dev)
+		for i in range(0, v):
+			x2[: ,: ,i, :] = x1
+			x3[: ,i ,:, :] = x1
+		x3 = torch.cat((x2,x3),dim=-1)
+		x3 = x3.reshape(n*v*v,128)
+		#print(x3.shape)
+		x3 = x3.float().to(dev)
+		x3 = self.l1(x3)
+		A1 = self.relu(x3)
+		#print(A1)
+		A1 = A1.reshape(n,v,v,1)
+		mask = mask.permute(0,2,3,1)
+		mask = mask.long()
+		#mask = mask.reshape(n,v,v)
+		#A1 = A1.reshape(n,v,v)
+		print(mask.shape)
+		print(A1.shape)
+		#print(A1)
+		A1[mask==0] = float('-inf')
+		#print(mask[1,: ,:])
+		#print(A1[1,:,:])
+		    
+		    
+		A1 = A1.reshape(n,v,v)
+		#for i in range(0, n):
+			
+		A1 = self.softmax(A1)
+		#p = A1
+		A1[A1!=A1] = 0
+		print(A1)
+		#print(A1)
+		A1 = A1.reshape(n,v,v,1)
+		A1 = A1.permute(0,3,1,2)
+		n, d, a, b = A1.size()
 
+
+		#A = A.reshape(n,v,v,7)
+		#A = A.permute(0,3,1,2)
+		#A = self.adjmatder(A[:,:7])
+		#x = self.conv(x)
 		# A is (n,64,v,v)
-		#A = A*mask
-		#Dl = ((A.sum(axis=2) + 0.001)**(-1)).float()
+		#A = A*mask#Dl = ((A.sum(axis=2) + 0.001)**(-1)).float()
 		# Dl is (n,64,v)
 		#A = torch.einsum('ncvw,ncw->ncvw',(A,Dl))
 		
 		# To increase the no of channels of the graph to out_channels*k
-		n, c, t, v = x.size()
-		# x is now a 5d tensor with size (N,k,c,t,v)
-		x1 = x.view(n*c*v, t)
-		x1 = x1.to(dev)
-		x1 = self.l2(x1)
-		x1 = x1.view(n,c,v)
-		x2 = torch.ones([n, c, v, v], dtype=torch.float64)
-		x2 = x2.to(dev)
-		for i in range(0, v):
-			x2[: ,: ,i, :] = x1
-		A = self.relu(x2)
+		
 
 		#print(x1.shape)
 		
 
-		A = self.softmax(A)
-		A = A.float().to(dev)
-		A = A*mask
+		#A = self.softmax(A)
+		#A = A*mask
 		#A is (n,64,v,v)
 		# Matrix multiplication followed by addition of all individual kernel matrices
 		#print(x.shape)
 		#print(A.shape)
-		x = torch.einsum('nctv,ncvw->nctw', (x, A))
+		x = torch.einsum('nctv,ndvw->nctw', (x, A1))
+		#A1 = A1.detach()
+		print(x.shape)
+		#print(x)
 
-		return x.contiguous(), A
+		return x.contiguous(), A1
+
 
